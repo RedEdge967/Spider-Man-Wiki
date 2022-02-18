f97c570e130f37ce5903bee03e7b3c06dc3f9205
Chandula Janith <91379432+RedEdge967@users.noreply.github.com>
2022-02-18 07:50:05 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,22 +8,22 @@ import styles from './styles.module.css';
 
 const features = [
   {
-    title: <>Easy to Use</>,
+    title: <>All about Spider Man</>,
     imageUrl: 'img/undraw_docusaurus_mountain.svg',
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        This wiki is all about the superhero Spider Man and made with ReactJS
+        using CSS3 and JavaScript with the help of docusaurus 2.
       </>
     ),
   },
   {
-    title: <>Focus on What Matters</>,
+    title: <>Open Source</>,
     imageUrl: 'img/undraw_docusaurus_tree.svg',
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        This Spider Man wiki is fully Open Source on Github and simply deployed to
+        the web with the help of vercel.
       </>
     ),
   },
@@ -32,8 +32,8 @@ const features = [
     imageUrl: 'img/undraw_docusaurus_react.svg',
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        This fanmade Spider Man wiki is fully powered by ReactJS and made using
+        docusaurus-2 made by Meta.
       </>
     ),
   },
@@ -59,7 +59,7 @@ function Home() {
   const {siteConfig = {}} = context;
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
+      title={`Welcome to ${siteConfig.title}`}
       description="Description will go into a meta tag in <head />">
       <header className={classnames('hero hero--primary', styles.heroBanner)}>
         <div className="container">
