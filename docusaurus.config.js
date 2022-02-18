a88766c8d141bad34bb192a44526935768152e9b
Chandula Janith <91379432+RedEdge967@users.noreply.github.com>
2022-02-18 14:07:45 +0530

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,7 +15,7 @@ module.exports = {
       },
       links: [
         {
-          to: 'docs/doc1',
+          to: 'docs/intro',
           activeBasePath: 'docs',
           label: 'Wiki',
           position: 'left',
