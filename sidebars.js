edb8dfe882a16ffb4b52b604ca842b93f794316b
Chandula Janith <91379432+RedEdge967@users.noreply.github.com>
2022-02-21 13:43:28 +0530

    Update sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -2,6 +2,6 @@ module.exports = {
   someSidebar: {
     Introduction: ['intro'],
     Wiki: ['wiki1', 'wiki2', 'wiki3'],
-    Villains: ['villain1'],
+    Villains: ['villain1', 'villain2'],
   },
 };
